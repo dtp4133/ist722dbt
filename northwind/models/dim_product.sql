38ecec0253bd90d2ead85fa7a38e3730443c14af
Dylan Phillips <dtphilli@g.syr.edu>
2025-10-26 18:13:38 +0000

    finished assignment f

---
+++ northwind/models/dim_product.sql
@@ -0,0 +1,18 @@
+with stg_products as (
+    select * from {{source("northwind","Products")}}
+),
+stg_suppliers as (
+    select * from {{source("northwind","Suppliers")}}
+),
+stg_categories as (
+    select * from {{source('northwind', 'Categories')}}
+)
+select {{ dbt_utils.generate_surrogate_key(['p.productid'])}} as productkey,
+    p.productid,
+    p.productname,
+    s.supplierid,
+    c.categoryname,
+    c.description as categorydescription
+from stg_products p 
+    left join stg_suppliers s on p.supplierid = s.supplierid
+    left join stg_categories c on p.categoryid = c.categoryid
